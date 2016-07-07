984da6e4104085c24677cb6edfa0ad7a165ba449
mbarvinok <nempak@gmail.com>
2016-07-07 09:10:09 +0300

    add migration

---
+++ migrations/m160707_060950_myDayResult.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160707_060950_myDayResult extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160707_060950_myDayResult cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
